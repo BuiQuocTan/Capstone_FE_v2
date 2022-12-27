b9498fdd74412759e617b69b76fe5ff25824d250
Thuận Tiger <ngocthuandn98@gmail.com>
2022-12-27 15:40:38 +0700

    agent

--- src/components/DataAgent.jsx
+++ src/components/DataAgent.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react'
 import '../style/admin.css'
 import SearchIcon from '@mui/icons-material/Search'
+import AddIcon from '@mui/icons-material/Add';
 import AgentAdmin from './AgentAdmin'
 import Paginate from 'react-paginate'
+import {db } from '../firebase'
+import {collection,doc,getDocs , deleteDoc} from "firebase/firestore"
 
 function DataAgent() {
   const [info, setInfo] = useState([])
@@ -18,50 +21,65 @@ function DataAgent() {
     setCurrentPage(selected)
   }
 
-  const loadData = async () => {
-    fetch('http://localhost:5000/api/listAgent', {
-      method: 'get',
-      headers: new Headers({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        'ngrok-skip-browser-warning': '69420',
-      }),
-    })
-      .then(async (res) => {
-        return res.json()
-      })
-      .then((receivedData) => setInfo(receivedData))
+  // const loadData = async () => {
+  //   Promise.all([
+  //     fetch('http://localhost:5000/api/listAgent', {
+  //       method: 'get',
+  //       headers: new Headers({
+  //         'Access-Control-Allow-Origin': '*',
+  //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  //         'ngrok-skip-browser-warning': '69420',
+  //       }),
+  //     }),
+  //     fetch('http://localhost:5000/api/home', {
+  //       method: 'get',
+  //       headers: new Headers({
+  //         'Access-Control-Allow-Origin': '*',
+  //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  //         'ngrok-skip-browser-warning': '69420',
+  //       }),
+  //     })
+  // ]).then(function (responses) {
+  //     // Get a JSON object from each of the responses
+  //     return Promise.all(responses.map(function (response) {
+  //         return response.json();
+  //     }));
+  // }).then(function (data) {
+  //     // Log the data to the console
+  //     // You would do something with both sets of data here
+  //     setInfo2(data)
+  // }).catch(function (error) {
+  //     // if there's an error, log it
+  //     console.log(error);
+  // });
+  //}
+  const loadData = async () =>{
+    await getDocs(collection(db, "listAgent"))
+            .then((querySnapshot)=>{               
+                const newData = querySnapshot.docs
+                    .map((doc) => ({...doc.data(), id:doc.id }));
+                  setInfo(newData);                
+            })
   }
-
+  
   useEffect(() => {
     loadData()
   }, [])
 
-  const deletePost = (id) => {
-    fetch(`http://localhost:5000/api/listAgent/${id}`, {
-      method: 'DELETE',
-      headers: new Headers({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        'ngrok-skip-browser-warning': '69420',
-      }),
-    })
-      .then((res) => {
-        res.json().then((resp) => {
-          alert('delete successful')
-        })
-        loadData()
-      })
-      .catch((err) => {
-        console.error(err)
-      })
+  const deletePost = async (id) => {
+    const reference = doc(db, 'listAgent',id)
+    await deleteDoc(reference)
+    .then( alert("Delete Successfull"))
   }
 
   return (
     <div>
+      <div className='admin-top'>
       <div className="searchAdmin">
         <input type="text" placeholder="Search..." name="search" />
         <SearchIcon />
+      </div>
+      <div className='add'><AddIcon /></div>
       </div>
       <div className="info-homeAdmin">
         <ul className="list-homeAdmin">
