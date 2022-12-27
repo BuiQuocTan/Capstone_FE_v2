05e242ab249c8b295d29a2a4900e847b29a4013c
Thuận Tiger <ngocthuandn98@gmail.com>
2022-12-27 16:24:22 +0700

    update

---
+++ src/components/AddAgent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../style/AddAgent.css'
+
+function AddAgent() {
+  return (
+    <div className="Add">AddAgent</div>
+  )
+}
+
+export default AddAgent
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,26 @@
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+// import { getAuth } from "firebase/auth"
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCvd3YqDppwfz8I0YhYhOxGOFJ_paheuWc",
+//     authDomain: "front-end-8a283.firebaseapp.com",
+//     projectId: "front-end-8a283",
+//     storageBucket: "front-end-8a283.appspot.com",
+//     messagingSenderId: "355889837337",
+//     appId: "1:355889837337:web:17e29b1a96752103918188"
+//   };
+
+//  const firebaseApp = firebase.initializeApp(firebaseConfig)
+//  const db = firebaseApp.firestore();
+// //  const auth = firebase.auth();
+// const auth = getAuth(firebaseApp);
+
+
+//   export { auth };
+//   export default db;
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
@@ -14,10 +37,9 @@ const firebaseConfig = {
   };
 
  const firebaseApp = firebase.initializeApp(firebaseConfig)
- const db = firebaseApp.firestore();
+ export const db = firebaseApp.firestore();
 //  const auth = firebase.auth();
 const auth = getAuth(firebaseApp);
 
 
   export { auth };
-  export default db;
